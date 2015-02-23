062b7b2fe5b6ea01053d66859013d02a51b3b0af
Val Savvateev <val@exitround.com>
2015-02-22 20:46:09 -0800

    updating examples

--- spec/capital-iq_spec.rb
+++ spec/capital-iq_spec.rb
@@ -1,30 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/capital-iq.rb')
 
 describe "CapitalIq" do
-  ci = CapitalIQ::Client.new(ENV['CAPIQ_USER'], ENV['CAPIQ_PWD'], Redis.new)
+  client = CapitalIQ::Client.new(ENV['CAPIQ_USER'], ENV['CAPIQ_PWD'])
 
   # Call based on a request object
-  req = CapitalIQ::Request.new(CapitalIQ::Functions::GDSHE, 'microsoft', 'IQ_COMPANY_ID_QUICK_MATCH')
-  res = ci.base_request(req)
+  # All requests passed to the base_request method are executed withing one roundtrip
+  req1 = CapitalIQ::Request.new(CapitalIQ::Functions::GDSHE, 'microsoft', 'IQ_COMPANY_ID_QUICK_MATCH')
+  req2 = CapitalIQ::Request.new(CapitalIQ::Functions::GDSHE, 'google', 'IQ_COMPANY_ID_QUICK_MATCH')
+  res = client.base_request([req1, req2])
 
-  # Shortened form
-  res = ci.request_gdshe('microsoft', 'IQ_COMPANY_ID_QUICK_MATCH', {startRank:1, endRank:20})
-  # Use 'scalar' if you query on a single identifier
-  # When using GDSHE or GDSHV, value for a given mnemonic will be an array (that's why we use 'first')
+  # Shortened form - no need to create request objects explicitly, although they do get created underneath:
+  res = client.request_gdshe('microsoft', 'IQ_COMPANY_ID_QUICK_MATCH', {startRank:1, endRank:20})
+
+  # Returned values are accessed by identifier and mnemonic:
+  res_val = res['microsoft']['IQ_COMPANY_ID_QUICK_MATCH']
+
+  # When using GDSHE or GDSHV, the result is represented by an array (so we'll use 'first' here)
+  ms_id = res_val.first
+
+  # You can use 'scalar' when querying on a single identifier, so we can shorten the whole thing to:
   ms_id = res.scalar['IQ_COMPANY_ID_QUICK_MATCH'].first
 
   # Multiple identifier / single mnemonic requests
-  res = ci.request_gdshe(['microsoft', 'google'], 'IQ_COMPANY_ID_QUICK_MATCH', {startRank:1, endRank:20})
+  # This example generates two requests
+  res = client.request_gdshe(['microsoft', 'google'], 'IQ_COMPANY_ID_QUICK_MATCH', {startRank:1, endRank:20})
   ms_id = res['microsoft']['IQ_COMPANY_ID_QUICK_MATCH'].first
-  g_id = res['google']['IQ_COMPANY_ID_QUICK_MATCH'].first
+  google_id = res['google']['IQ_COMPANY_ID_QUICK_MATCH'].first
 
-
-  # Mulple identifier / multiple mnemonic queries
-  res = ci.request_gdsp([ms_id, g_id], %w(IQ_COMPANY_WEBSITE IQ_COMPANY_NAME IQ_BUSINESS_DESCRIPTION))
-  # Use the mnemonic name to retrieve its value or 'to_hash' to retrieve all mnemonic values for a given identifier
-  ms_name = res[ms_id]['IQ_COMPANY_NAME'] # signle value
+  # Multiple identifier / multiple mnemonic queries
+  # This call generates (2 identifiers times 3 mnemonics) = 6 requests (they're still executed in a batch)
+  res = client.request_gdsp([ms_id, google_id], %w(IQ_COMPANY_WEBSITE IQ_COMPANY_NAME IQ_BUSINESS_DESCRIPTION))
+  # You can use the 'to_hash' method to retrieve all mnemonic values for a given identifier
   ms_data = res[ms_id].to_hash # all mnemonics for ms_id
-  g_data = res[g_id].to_hash # all mnemonics for g_id
-
-  # puts ms_data, g_data
+  # Or you can retrieve all values for all identifiers
+  google_data = res[google_id].to_hash # all mnemonics for google_id
 end
